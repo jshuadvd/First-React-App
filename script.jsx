9917385e4594e29486d63b78df21519e6fc7c3d8
Joshua David <jshuadvd@icloud.com>
2015-09-10 16:08:59 -0700

    definine the handleClick function

--- script.jsx
+++ script.jsx
@@ -2,9 +2,12 @@ var Button = React.createClass({
 getInitialState: function() {
   return {counter: 0};
 },
+handleClick : function() {
+
+}
 render: function() {
   return (
-  <button onClick={}>{this.state.counter}</button>
+  <button onClick={this.handleClick}>{this.state.counter}</button>
   )
  }
 });
