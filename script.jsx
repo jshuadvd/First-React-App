7a8ac0825a01319342f42a2f7a48e8901165d712
jshuadvd <jshuadvd@icloud.com>
2015-09-12 23:22:41 -0700

    Corrected syntax error

--- script.jsx
+++ script.jsx
@@ -1,13 +1,12 @@
 var Button = React.createClass({
-
-localHandleClick : function( {
-  this.props.localHandleClick(this.props.increment);
-},
-render: function() {
-  return (
-  <button onClick={this.localHandleClick}>+{this.props.increment}</button>
-  )
-}
+  localHandleClick : function() {
+    this.props.localHandleClick(this.props.increment);
+  },
+  render: function() {
+    return (
+    <button onClick={this.localHandleClick}>+{this.props.increment}</button>
+    )
+  }
 });
 
 var Result = React.createClass({
