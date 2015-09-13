2447c6c677f9fd8cdb06e1831e01349148e0ff9f
JSHUADVD <joshuadavid@jshuadvd.local>
2015-09-12 22:55:07 -0700

    Getting the localClickHandler set to call the increment

--- script.jsx
+++ script.jsx
@@ -1,8 +1,12 @@
 var Button = React.createClass({
 
+localHandleClick : function( {
+  this.props.localHandleClick(this.props.increment);
+
+})
 render: function() {
   return (
-  <button onClick={this.props.localHandleClick}>+{this.props.increment}</button>
+  <button onClick={this.localHandleClick}>+{this.props.increment}</button>
   )
  }
 });
@@ -28,11 +32,11 @@ var Main = React.createClass({
   render: function() {
     return (
       <div>
-        <Button localHandleClick={this.handleClick} increment={1}/>
-        <Button localHandleClick={this.handleClick} increment={24}/>
-        <Button localHandleClick={this.handleClick} increment={48}/>
-        <Button localHandleClick={this.handleClick} increment={12}/>
-        <Result localCounter={this.state.counter}/>
+      <Button localHandleClick={this.handleClick} increment={1}/>
+      <Button localHandleClick={this.handleClick} increment={24}/>
+      <Button localHandleClick={this.handleClick} increment={48}/>
+      <Button localHandleClick={this.handleClick} increment={12}/>
+      <Result localCounter={this.state.counter}/>
       </div>
     )
   }
